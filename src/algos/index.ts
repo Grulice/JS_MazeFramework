697da1cc7c3eb8fb8c9f581f60f3643901905ad4
Javid Bunyadzade <j.bunyadzade@gmail.com>
2021-03-26 23:55:44 +0400

    add position interpolation for drawing

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -2,6 +2,13 @@ import { IMazeAlgorithm } from "../types";
 
 export const workers: IMazeAlgorithm[] = [
     {
+        name: "wave_propagation",
+        worker: new Worker(
+            new URL("./scripts/wave_propagation.js", import.meta.url)
+        ),
+        displayName: "Wave Propagation",
+    },
+    {
         name: "astar",
         worker: new Worker(new URL("./scripts/astar.js", import.meta.url)),
         displayName: "A*",
@@ -10,12 +17,5 @@ export const workers: IMazeAlgorithm[] = [
         name: "mrgd",
         worker: new Worker(new URL("./scripts/mrgd.js", import.meta.url)),
         displayName: "MRGD",
-    },
-    {
-        name: "wave_propagation",
-        worker: new Worker(
-            new URL("./scripts/wave_propagation.js", import.meta.url)
-        ),
-        displayName: "Wave Propagation",
     },
 ];
